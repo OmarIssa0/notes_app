dad0cb7429cefe07429e55b18150158eaaec3f3b
OmarIssa0 <omar.issa.slwade@gmail.com>
2023-03-06 23:06:02 +0300

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,13 @@
+class NoteModel {
+  final String title;
+  final String subTitle;
+  final String dete;
+  final int color;
+
+  NoteModel({
+    required this.title,
+    required this.subTitle,
+    required this.dete,
+    required this.color,
+  });
+}
