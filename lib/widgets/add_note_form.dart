b91b16ba98984c55847e584310b6d7214dc0b75b
OmarIssa0 <omar.issa.slwade@gmail.com>
2023-03-07 22:27:33 +0300

    refactor multi bloc provider

--- lib/widgets/add_note_form.dart
+++ lib/widgets/add_note_form.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:notes_app/models/note_model.dart';
 
 import 'custom_button.dart';
 import 'custom_text_filed.dart';
@@ -40,7 +43,7 @@ class _AddNoteFormState extends State<AddNoteForm> {
           ),
           CustomTextFormFiled(
             onSaved: (value) {
-              value = subTitle;
+              subTitle = value;
             },
             hint: 'content',
             maxLines: 5,
@@ -52,6 +55,13 @@ class _AddNoteFormState extends State<AddNoteForm> {
             onTap: () {
               if (formKey.currentState!.validate()) {
                 formKey.currentState!.save();
+                var noteModel = NoteModel(
+                  title: title!,
+                  subTitle: subTitle!,
+                  dete: DateTime.now().toString(),
+                  color: Colors.blue.value,
+                );
+                BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
               } else {
                 autovalidateMode = AutovalidateMode.always;
                 setState(() {});
